0b1962dd56cc669789b27d737cc8cdc862a2b9b1
Farhad Mortezapour <farhadmp@gmail.com>
2025-11-10 10:34:17 +0330

    New changes

---
+++ script.js
@@ -0,0 +1,9 @@
+// انیمیشن ساده برای اسکرول نرم
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
